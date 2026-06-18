71420bcd3dbd94fa56f460f57516d3a06f4565da
A.Lejeune <acelyalejeune@gmail.com>
2026-06-18 15:02:34 +0200

    Initial Flowdeck setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1>Flowdeck</h1>
+      <p>Creative project management, without the chaos.</p>
+      <button>Add prohect</button>
+    </main>
+  );
+}
+
+export default App;
